9799d8a82f690a14fdf4c71996089bea23943333
mrsvolodya <volodyamurskyi@gmail.com>
2025-07-20 00:26:07 +0300

    Finish mocks test task

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,10 +1,26 @@
-'use strict';
+"use strict";
 
-describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+describe("ifElse", () => {
+  const { ifElse } = require("./ifElse");
 
-  it('should ', () => {
+  it("should run first function if condition is true", () => {
+    const condition = jest.fn(() => true);
+    const first = jest.fn(() => "first");
+    const second = jest.fn(() => "second");
 
+    ifElse(condition, first, second);
+    expect(first).toHaveBeenCalledTimes(1);
+    expect(second).not.toHaveBeenCalled();
+  });
+
+  it("should run second function if condition is false", () => {
+    const condition = jest.fn(() => false);
+    const first = jest.fn(() => "first");
+    const second = jest.fn(() => "second");
+
+    ifElse(condition, first, second);
+    expect(first).not.toHaveBeenCalled();
+    expect(second).toHaveBeenCalledTimes(1);
   });
 
   // write tests here
